08dbb31c3b9739c2098dedf68bae8e43492b1284
Maanas Arora <maanasarora23@gmail.com>
2021-01-15 00:42:32 -0500

    add book information models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(model.Model):
+    title = models.CharField(max_length=200)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    year = models.IntegerField()
